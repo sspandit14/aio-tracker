25fff9a7858bf773e1ad6c7c57e5a7994b0519f2
sspandit14 <sspandit@usc.edu>
2026-07-16 13:43:14 +0800

    docker postgres setup

--- reset.sql
+++ reset.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS items;
 DROP TYPE IF EXISTS item_status;
 
-\i migrations/init.sql
-\i seed.sql
+\i /docker-entrypoint-initdb.d/01-init.sql
+\i /docker-entrypoint-initdb.d/02-seed.sql
+
+COMMIT;
